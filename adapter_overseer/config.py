5f983b539c0e407f93d9d9cbea77ee3b73b20880
wassname <github@wassname.org>
2024-06-11 21:25:28 +0800

    it seemed to work!

--- adapter_overseer/config.py
+++ adapter_overseer/config.py
@@ -10,10 +10,10 @@ ROOT_DIR = Path(__file__).parent.parent
 class ExtractConfig(Serializable):
     """Config for extracting hidden states from a language model."""
 
-    datasets: tuple[str, ...] = ("amazon_polarity", )
+    datasets: tuple[str, ...] = ("amazon_polarity", "glue:qnli")
     """datasets to use, e.g. `"super_glue:boolq"` or `"imdb"` `"glue:qnli` super_glue:rte super_glue:axg sst2 hans"""
 
-    datasets_ood: tuple[str, ...] = ( 'imdb')
+    datasets_ood: tuple[str, ...] = ( 'imdb', "super_glue:boolq")
     """Out Of Distribution datasets to use, e.g. `"super_glue:boolq"` or `"imdb"` `"glue:qnli"""
     
     model: str = "failspy/Llama-3-8B-Instruct-abliterated"
@@ -21,7 +21,7 @@ class ExtractConfig(Serializable):
     collection_layers: tuple[str, ...] = ("base_model.model.model.layers.10", "base_model.model.model.layers.20", )
     # """Names of layers to extract from using baukit.nethook.TraceDict"""
 
-    batch_size: int = 2
+    batch_size: int = 4
 
     prompt_format: str | None = None
     """if the tokenizer does not have a chat template you can set a custom one. see src/prompts/templates/prompt_formats/readme.md."""
@@ -32,7 +32,7 @@ class ExtractConfig(Serializable):
     max_length: int | None = 776
     """Maximum length of the input sequence passed to the tokenize encoder function"""
 
-    max_examples: tuple[int, int] = 1000
+    max_examples: tuple[int, int] = 3000
     """Maximum number of examples"""
 
     seed: int = 42
